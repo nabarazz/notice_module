a4e3024206f10e46e7e87acb5fdc21ab9e2e0438
unknown <nabarazz01@outlook.com>
2024-08-12 21:51:03 +0545

    mo

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -2,4 +2,4 @@
 
 # Instead of importing the module directly, we can import the controllers
 # later when they are needed, thus avoiding circular import issues.
-from . import main
+# from . import main
--- models/notice.py
+++ models/notice.py
@@ -1,13 +1,12 @@
+# models/notice.py
 from odoo import models, fields
 
 
-class Notice(models.Model):
+class WebsiteNotice(models.Model):
     _name = "website.notice"
     _description = "Notice"
 
     title = fields.Char(string="Title", required=True)
     subtitle = fields.Char(string="Subtitle")
-    attachment_id = fields.Many2one(
-        "ir.attachment", string="File Attachment", required=True
-    )
+    attachment_id = fields.Many2one("ir.attachment", string="Attachment")
     published = fields.Boolean(string="Published", default=True)
